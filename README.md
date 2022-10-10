b6d75d8eba6c9e4a5c1a5a8fb63568f7f7dbe9ba
VHSgunzo <wosganzo@gmail.com>
2022-10-10 18:45:19 +0300

    v4.9.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # findutils-static
-Statically compiled findutils with musl
+* Statically compiled findutils with musl
+## To get started:
+* **Download the latest revision**
+```
+git clone https://github.com/VHSgunzo/findutils-static.git
+```
+* **Build**
+```
+cd findutils-static && ./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+export MAKEFLAGS="-j$(nproc)"
+
+# WITH_UPX=1
+# NO_SYS_MUSL=1
+
+findutils_version="latest"
+musl_version="latest"
+
+platform="$(uname -s)"
+platform_arch="$(uname -m)"
+
+[ "$findutils_version" == "latest" ] && \
+  findutils_version="$(curl -s https://ftp.gnu.org/gnu/findutils/|tac|\
+                       grep -om1 'findutils-.*\.tar\.xz'|cut -d'>' -f2|sed 's|findutils-||g;s|.tar.xz||g')"
+
+[ "$musl_version" == "latest" ] && \
+  musl_version="$(curl -s https://www.musl-libc.org/releases/|tac|grep -v 'latest'|\
+                  grep -om1 'musl-.*\.tar\.gz'|cut -d'>' -f2|sed 's|musl-||g;s|.tar.gz||g')"
+
+if [ -d build ]
+    then
+        echo "= removing previous build directory"
+        rm -rf build
+fi
+
+if [ -d release ]
+    then
+        echo "= removing previous release directory"
+        rm -rf release
+fi
+
+# create build and release directory
+mkdir build
+mkdir release
+pushd build
+
+# download tarballs
+echo "= downloading findutils v${findutils_version}"
+curl -LO https://ftp.gnu.org/gnu/findutils/findutils-${findutils_version}.tar.xz
+
+echo "= extracting findutils"
+tar -xJf findutils-${findutils_version}.tar.xz
+
+if [ "$platform" == "Linux" ]
+    then
+        echo "= setting CC to musl-gcc"
+        if [[ ! -x "$(which musl-gcc 2>/dev/null)" || "$NO_SYS_MUSL" == 1 ]]
+            then
+                echo "= downloading musl v${musl_version}"
+                curl -LO https://www.musl-libc.org/releases/musl-${musl_version}.tar.gz
+
+                echo "= extracting musl"
+                tar -xf musl-${musl_version}.tar.gz
+
+                echo "= building musl"
+                working_dir="$(pwd)"
+
+                install_dir="${working_dir}/musl-install"
+
+                pushd musl-${musl_version}
+                env CFLAGS="$CFLAGS -Os -ffunction-sections -fdata-sections" LDFLAGS='-Wl,--gc-sections' ./configure --prefix="${install_dir}"
+                make install
+                popd # musl-${musl-version}
+                export CC="${working_dir}/musl-install/bin/musl-gcc"
+            else
+                export CC="$(which musl-gcc 2>/dev/null)"
+        fi
+        export CFLAGS="-static -static-libgcc -static-libstdc++"
+        export LDFLAGS='--static'
+    else
+        echo "= WARNING: your platform does not support static binaries."
+        echo "= (This is mainly due to non-static libc availability.)"
+fi
+
+echo "= building findutils"
+pushd findutils-${findutils_version}
+env CFLAGS="$CFLAGS -g -O2 -Os -ffunction-sections -fdata-sections" LDFLAGS="$LDFLAGS -Wl,--gc-sections" ./configure
+make
+popd # findutils-${findutils_version}
+
+popd # build
+
+shopt -s extglob
+
+echo "= extracting findutils binary"
+for file in {find,xargs,locate,frcode,updatedb}
+    do
+        mv "build/findutils-${findutils_version}/$file/$file" release 2>/dev/null
+done
+for file in {locate,frcode,updatedb}
+    do
+        mv "build/findutils-${findutils_version}/locate/$file" release 2>/dev/null
+done
+
+echo "= striptease"
+for file in release/*
+  do
+      strip -s -R .comment -R .gnu.version --strip-unneeded "$file" 2>/dev/null
+done
+
+if [[ "$WITH_UPX" == 1 && -x "$(which upx 2>/dev/null)" ]]
+    then
+        echo "= upx compressing"
+        for file in release/*
+          do
+              upx -9 --best "$file" 2>/dev/null
+        done
+fi
+
+echo "= create release tar.xz"
+tar --xz -acf findutils-static-v${findutils_version}-${platform_arch}.tar.xz release
+
+if [ "$NO_CLEANUP" != 1 ]
+    then
+        echo "= cleanup"
+        rm -rf release build
+fi
+
+echo "= findutils v${findutils_version} done"
